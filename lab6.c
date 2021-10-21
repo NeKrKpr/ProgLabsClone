79a58854a9859e5dc822211df697d51e6ba89bd8
Kirill <87267763+KrKpr@users.noreply.github.com>
2021-10-21 22:27:23 +0300

    Add files via upload

---
+++ lab6.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main(void) {
+    int i;
+
+    //TASK 1
+    int arr[4];
+    int *p = arr;
+    for (i = 0; i < 4; i++)
+        scanf("%d", (p + i));
+    printf("\n");
+    for (i = 0; i < 4; i++)
+        printf("%d ", *(p + i));
+    printf("\n\n");
+
+    //TASK 2
+    int *mass;
+    mass = (int *) malloc(4 * sizeof(int));
+    for (i = 0; i < 4; i++)
+        scanf("%d", &mass[i]);
+    for (i = 0; i < 4; i++)
+        printf("%d ", mass[i]);
+    free(mass);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
