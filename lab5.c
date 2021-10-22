3de52a6bb1680ab67dd8f3703302777659df4121
Kirill <87267763+KrKpr@users.noreply.github.com>
2021-10-22 08:36:33 +0300

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
-int main (void){
+int main(void) {
     int i, j;
     //TASK1
     int mass1[7] = {0, 3, 5, 7, 11, 13};
-    for (i =0; i<7; i++)
+    for (i = 0; i < 7; i++){
         printf("%d ", mass1[i]);
+    }
     printf("\n\n");
 
     //TASK2
-    int arr1[2][2] = {1, 0,
-                      1, 4};
-    int arr2[2][2] = {1, 2,
-                      0, 1};
-    int arr3[2][2] = {0}; //arr3 = arr1 * arr2
-    for(int i = 0; i < 2; i++)
-        for(int j = 0; j < 2; j++)
-            for(int k = 0; k < 2; k++)
-                arr3[i][j] += arr1[i][k] *arr2[k][j];
-
-    for(i = 0; i < 2; i++){
-        for (j = 0; j < 2; j++)
+    int arr1[2][2] = {1, 0, 1, 4};
+    int arr2[2][2] = {1, 2, 0, 1};
+    int arr3[2][2] = {0};
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            for (int k = 0; k < 2; k++){
+                arr3[i][j] += arr1[i][k] * arr2[k][j];
+            }
+    
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++){
             printf("%d ", arr3[i][j]);
+        }
         printf("\n");
     }
 }
-
 
 
 
